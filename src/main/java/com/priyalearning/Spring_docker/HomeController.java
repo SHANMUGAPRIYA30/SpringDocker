304e31ae65abb296eb581e684ac63e6809f7ab81
Shanmuga Priya <shammymit@gmail.com>
2022-04-27 14:48:45 +0530

    initial commit

---
+++ src/main/java/com/priyalearning/Spring_docker/HomeController.java
@@ -0,0 +1,14 @@
+package com.priyalearning.Spring_docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		
+		return "Creating springboot project and docker file and pushing it to docker hub";
+	}
+}
